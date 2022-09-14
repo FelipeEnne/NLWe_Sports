150fddc6a71dce2db7288fd073096dc5fc6c9caf
Felipe Enne <felipeenne@gmail.com>
2022-09-14 18:20:35 -0300

    change mobile

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (req, res) => {
-  return res.json([
+app.get("/game", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/ads", (request, response) => {
+  return response.json([
     { id: 1, name: "Anúncio 1" },
     { id: 2, name: "Anúncio 2" },
     { id: 3, name: "Anúncio 3" },
